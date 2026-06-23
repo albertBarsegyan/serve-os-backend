3a002d989afafa7d41cc5cd46b657050c8cb3b0b
Your Name <you@example.com>
2026-06-23 15:40:36 +0400

    fix cookie

--- src/common/utils/business.utils.ts
+++ src/common/utils/business.utils.ts
@@ -4,10 +4,12 @@ export const setBusinessCookie = ({
   res,
   businessId,
   isProduction,
+  domain,
 }: {
   res: Response;
   businessId: string;
   isProduction: boolean;
+  domain?: string;
 }) => {
   res.cookie('business_id', businessId, {
     httpOnly: true,
@@ -15,14 +17,16 @@ export const setBusinessCookie = ({
     secure: isProduction,
     maxAge: 30 * 24 * 60 * 60 * 1000, // 30 days
     path: '/',
+    ...(domain ? { domain } : {}),
   });
 };
 
-export const clearBusinessCookie = (res: Response, isProduction: boolean) => {
+export const clearBusinessCookie = (res: Response, isProduction: boolean, domain?: string) => {
   res.clearCookie('business_id', {
     httpOnly: true,
     sameSite: 'lax',
     secure: isProduction,
     path: '/',
+    ...(domain ? { domain } : {}),
   });
 };
